9db509e8d2ee83f2515cf102b368dac68ea172f4
Patrick Bennett <pbennettwa@gmail.com>
2019-12-12 18:34:39 -0800

    render all modules from aws

--- server/index.js
+++ server/index.js
@@ -10,19 +10,19 @@ app.use(express.static(path.resolve(__dirname, '..', 'public')));
 app.use(express.json());
 
 app.get('/kyle-service/bundle.js', (req, res) => {
-  request('http://localhost:3001/bundle.js', (err, response) => {
+  request('http://34.213.221.210:3001/bundle.js', (err, response) => {
     res.send(response.body);
   });
 });
 
 app.get('/api/listings/:Listing_id', (req, res) => {
-  request('http://localhost:3001/api/listings/' + req.params.Listing_id, (err, response) => {
+  request('http://34.213.221.210:3001/api/listings/' + req.params.Listing_id, (err, response) => {
     res.send(response.body);
   });
 });
 
 app.get('/patrick-service/bundle.js', (req, res) => {
-  request('http://localhost:3002/bundle.js', (err, response) => {
+  request('http://18.236.158.99:3002/bundle.js', (err, response) => {
     if (err) {
       console.log(err);
     }
@@ -31,7 +31,7 @@ app.get('/patrick-service/bundle.js', (req, res) => {
 });
 
 app.get('/listings/:id', (req, res) => {
-  request('http://localhost:3002/listings/' + req.params.id, (err, response) => {
+  request('http://18.236.158.99:3002/listings/' + req.params.id, (err, response) => {
     if (err) {
       console.log(err);
     }
@@ -40,7 +40,7 @@ app.get('/listings/:id', (req, res) => {
 });
 
 app.get('/sayer-service/bundle.js', (req, res) => {
-  request('http://localhost:3003/bundle.js', (err, response) => {
+  request('http://18.221.25.146:3003/bundle.js', (err, response) => {
     if (err) {
       console.log(err);
     }
@@ -49,7 +49,7 @@ app.get('/sayer-service/bundle.js', (req, res) => {
 });
 
 app.get('/api/listing/:Listing_id/', (req, res) => {
-  request('http://localhost:3003/api/listing/' + req.params.Listing_id, (err, response) => {
+  request('http://18.221.25.146:3003/api/listing/' + req.params.Listing_id, (err, response) => {
     if (err) {
       console.log(err);
     }
@@ -57,20 +57,20 @@ app.get('/api/listing/:Listing_id/', (req, res) => {
   });
 });
 
-// app.get('/matt-service/bundle.js', (req, res) => {
-//   request('http://localhost:3004/bundle.js', (err, response) => {
-//     res.send(response.body);
-//   });
-// });
+app.get('/matt-service/bundle.js', (req, res) => {
+  request('http://54.202.99.13:3004/bundle.js', (err, response) => {
+    res.send(response.body);
+  });
+});
 
-// app.get('/api/listings/:listing_id/', (req, res) => {
-//   request('http://localhost:3004/api/listings/' + req.params.listing_id, (err, response) => {
-//     res.send(response.body);
-//   });
-// });
+app.get('/api/houseprices/:listing_id/', (req, res) => {
+  request('http://54.202.99.13:3004/api/houseprices/' + req.params.listing_id, (err, response) => {
+    res.send(response.body);
+  });
+});
 
 app.get('/inna-service/bundle.js', (req, res) => {
-  request('http://localhost:3005/dist/bundle.js', (err, response) => {
+  request('http://35.164.99.110:3005/dist/bundle.js', (err, response) => {
     // console.log('response ', response.body);
 
     if (err) {
@@ -81,7 +81,7 @@ app.get('/inna-service/bundle.js', (req, res) => {
 });
 
 app.get('/api/photos/:id/', (req, res) => {
-  request('http://localhost:3005/api/photos/' + req.params.id, (err, response) => {
+  request('http://35.164.99.110:3005/api/photos/' + req.params.id, (err, response) => {
     if (err) {
       console.log(err);
     }
